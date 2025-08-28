ae84209dd12bbe06908d538640d610fa14574c49
TuNombreDeUsuario <tuemail@example.com>
2025-08-27 21:52:01 -0500

    crear repository y lasclases  de servicios, y el metodo show de productoController

---
+++ spring-ecommerce/src/main/java/com/cursos/ecommerce/spring_ecommerce/services/ProductoService.java
@@ -0,0 +1,13 @@
+package com.cursos.ecommerce.spring_ecommerce.services;
+
+import com.cursos.ecommerce.spring_ecommerce.models.Producto;
+
+import java.util.Optional;
+
+public interface ProductoService {
+    public Producto save(Producto producto) ;
+    public Optional<Producto> get(Long id);
+    public void update(Producto producto);
+    public void delete(Long id);
+
+}
