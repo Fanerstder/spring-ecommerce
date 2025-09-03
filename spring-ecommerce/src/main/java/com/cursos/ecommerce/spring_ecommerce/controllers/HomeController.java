bf8cc61f89784a207abf14b66e7b897e967cbdfd
TuNombreDeUsuario <tuemail@example.com>
2025-09-03 13:00:16 -0500

    funcionalidad de la vista productoHome

--- spring-ecommerce/src/main/java/com/cursos/ecommerce/spring_ecommerce/controllers/HomeController.java
+++ spring-ecommerce/src/main/java/com/cursos/ecommerce/spring_ecommerce/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.cursos.ecommerce.spring_ecommerce.controllers;
 
+import com.cursos.ecommerce.spring_ecommerce.models.Producto;
 import com.cursos.ecommerce.spring_ecommerce.services.ProductoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -26,8 +29,13 @@ public class HomeController {
     }
 
     @GetMapping("productohome/{id}")
-    public String productoHome(@PathVariable Long id) {
+    public String productoHome(@PathVariable Long id, Model model) {
         log.info("Id del producto enviado como parámetro {}", id);
+        Producto producto = new Producto();
+        Optional<Producto> productoOptional = productoService.get(id);
+        producto = productoOptional.get();
+
+        model.addAttribute("producto", producto);
         return "usuario/productohome";
     }
 }
