1db399566c5ec2b08a63a9680a78b883c3b9de8f
TuNombreDeUsuario <tuemail@example.com>
2025-09-09 13:15:04 -0500

    funcionalidad para guardar el registro de usuarios

---
+++ spring-ecommerce/src/main/java/com/cursos/ecommerce/spring_ecommerce/controllers/UsuarioController.java
@@ -0,0 +1,40 @@
+package com.cursos.ecommerce.spring_ecommerce.controllers;
+
+import com.cursos.ecommerce.spring_ecommerce.models.Usuario;
+import com.cursos.ecommerce.spring_ecommerce.services.IUsuarioService;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    private final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
+
+    @Autowired
+    private IUsuarioService usuarioService;
+
+    @GetMapping("/registro")
+    public String create() {
+        return "usuario/registro";
+    }
+
+    @PostMapping("/save")
+    public String save(Usuario usuario) {
+
+        logger.info("Usuario registro: {}", usuario);
+        usuario.setTipo("USER");
+        usuarioService.save(usuario);
+        return "redirect:/";
+    }
+
+}
