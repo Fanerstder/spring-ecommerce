1619c73da44e4f9803d8595bfc71a91fa64be59a
Fanerstder <fanerstder@gmail.com>
2025-09-13 13:46:02 -0500

    funcionalidad ver detalle de compras al user

--- spring-ecommerce/src/main/java/com/cursos/ecommerce/spring_ecommerce/services/IOrdenService.java
+++ spring-ecommerce/src/main/java/com/cursos/ecommerce/spring_ecommerce/services/IOrdenService.java
@@ -5,10 +5,12 @@ import com.cursos.ecommerce.spring_ecommerce.models.Usuario;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface IOrdenService {
     List<Orden> findAll();
+    Optional<Orden> findById(Long id);
     Orden save(Orden orden);
     String generarNumeroOrden();
     List<Orden> findByUsuario (Usuario usuario);
